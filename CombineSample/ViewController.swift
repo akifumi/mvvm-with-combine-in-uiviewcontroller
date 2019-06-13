a18194683565e33f18140bba1dce7901d57e79e5
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-06-13 11:04:51 +0900

    Created ViewModel.

--- CombineSample/ViewController.swift
+++ CombineSample/ViewController.swift
@@ -7,6 +7,53 @@
 //
 
 import UIKit
+import Combine
+
+final class ViewModel {
+    struct StatusText {
+        let content: String
+        let color: UIColor
+    }
+    let statusSubject = CurrentValueSubject<StatusText, Never>(StatusText(content: "NG", color: .red))
+
+    let usernameSubject = CurrentValueSubject<String?, Never>(nil)
+    private var validatedUsername: AnyPublisher<String?, Never> {
+        return usernameSubject
+//            .debounce(for: 0.5, scheduler: RunLoop.main)
+//            .removeDuplicates()
+            .flatMap { (username) -> AnyPublisher<String?, Never> in
+                Publishers.Future<String?, Never> { (promise) in
+                    // FIXME: API request
+                    guard let username = username else {
+                        promise(.success(nil))
+                        return
+                    }
+                    if 1...10 ~= username.count {
+                        promise(.success(username))
+                    } else {
+                        promise(.success(nil))
+                    }
+                }
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func viewDidLoad() {
+        // Update StatusText
+        _ = validatedUsername
+            .map { (value) -> StatusText in
+                if let _ = value {
+                    return StatusText(content: "OK", color: .green)
+                } else {
+                    return StatusText(content: "NG", color: .red)
+                }
+            }
+            .sink { [weak self] (value) in
+                self?.statusSubject.send(value)
+            }
+    }
+}
 
 class ViewController: UIViewController {
     @IBOutlet private var statusLabel: UILabel!
